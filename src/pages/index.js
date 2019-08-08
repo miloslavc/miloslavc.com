4f864aedffd799234ca78d1be09cbb4d9a472593
Miloslav Cvetkovic <miloslav.cve@gmail.com>
2019-08-08 23:12:20 +0200

    Initial Release

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,7 @@ import SEO from "../components/seo"
 import Hero from "../components/hero"
 import Process from "../components/process"
 import Featured from "../components/featured"
-<<<<<<< HEAD
 import Testimonial from "../components/testimonial"
-=======
-import Testimonial from "../components/testimonial"
->>>>>>> e7abdfe47a73c5594d3c7f54efd7630f98e63b05
 
 function IndexPage() {
   const ref = createRef()
